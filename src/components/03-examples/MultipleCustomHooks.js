ecb74c24147dfaf4aa7b7833b4795394694fef74
yimialmonte <yelmialmonte@gmail.com>
2020-09-14 11:58:22 -0400

    combine multiple hooks

---
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useFetch } from '../../hooks/useFetch'
+import '../02-useEffect/SimpleForm'
+export const MultipleCustomHooks = () => {
+  const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+  console.log(state);
+  return (
+    <div>
+      <h1>Custom Hooks</h1>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWithCustomHooks } from './components/02-useEffect/FormWithCustomHooks';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
-ReactDOM.render(<FormWithCustomHooks />, document.getElementById('root'));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'));
