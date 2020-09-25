de78497b7c7d9e13db3fac5b3b9001928b266313
yimialmonte <yelmialmonte@gmail.com>
2020-09-25 16:17:54 -0400

    create app for using use context

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(<MainApp />, document.getElementById('root'));
 
 // import './components/08-useReducer/intro-reducer'
