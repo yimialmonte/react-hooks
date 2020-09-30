8d205435eed0011e6e6a9b885b4e9fd556b5cc7d
yimialmonte <yelmialmonte@gmail.com>
2020-09-30 06:05:06 -0400

    complete test cases for use counter hook

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,14 +1,54 @@
-const { renderHook } = require("@testing-library/react-hooks")
-const { useCounter } = require("../../hooks/useCounter")
+const { renderHook, act } = require('@testing-library/react-hooks');
+const { useCounter } = require('../../hooks/useCounter');
 
 describe('Test on useCounter hooks', () => {
-
   test('should return a value by default', () => {
     const { result } = renderHook(() => useCounter());
     expect(result.current.counter).toBe(10);
     expect(typeof result.current.descrement).toBe('function');
     expect(typeof result.current.increment).toBe('function');
     expect(typeof result.current.reset).toBe('function');
-  })
+  });
 
-})
+  test('should have a counter value equal to the value', () => {
+    const value = 100;
+    const { result } = renderHook(() => useCounter(value));
+    expect(result.current.counter).toBe(value);
+  });
+
+  test('should increment the counter by one', () => {
+    const value = 100;
+    const { result } = renderHook(() => useCounter(value));
+    const { increment } = result.current;
+    act(() => {
+      increment();
+    });
+
+    const { counter } = result.current;
+
+    expect(counter).toBe(value + 1);
+  });
+
+  test('should descrement the counter by one', () => {
+    const value = 100;
+    const { result } = renderHook(() => useCounter(value));
+    const { descrement } = result.current;
+    act(() => {
+      descrement();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(value - 1);
+  });
+
+  test('should reset value as the initial', () => {
+    const initialValue = 10;
+    const { result } = renderHook(() => useCounter(initialValue));
+    const { increment, reset } = result.current;
+    act(() => {
+      increment();
+      reset();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(initialValue);
+  });
+});
